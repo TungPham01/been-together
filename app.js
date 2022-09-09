dc4a48c58ca6c0a55675f828377e59e8c8f2fe7d
Tung Pham <phamduytung11@gmail.com>
2022-09-09 16:22:37 +0700

    improve code

--- app.js
+++ app.js
@@ -9,13 +9,32 @@ document.addEventListener('DOMContentLoaded', function () {
       var rootTime = document.querySelector("time");
       document.querySelector("anniv").textContent = `${(yourDate.getDate() > 9) ? yourDate.getDate() : "0" + yourDate.getDate()}-${(yourDate.getMonth() > 8) ? (yourDate.getMonth() + 1) : "0" + (yourDate.getMonth() + 1)}-${yourDate.getFullYear()}`;
 
-      document.querySelector("date").textContent = Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24) + " Ngày";
+      let year = Math.floor(Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24) / 365);
+      let month = Math.floor(Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24) / 30);
+      let hourDate = Math.floor(Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24) * 24);
+      let minuteDate = Math.floor(Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24) * 24 * 60)
+      let secondDate = Math.floor(Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24) * 24 * 60 * 60)
+
+      document.querySelector("date").textContent =
+            year + " Năm - " + month + " Tháng - " +
+            Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24) + " Ngày"
+            // + hourDate + " Giờ - "
+            // + minuteDate + " Phút - "
+            // + secondDate + " Giây";
+
+      console.log('NĂM' + Math.floor(Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24) / 365))
+      console.log('THÁNG' + Math.floor(Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24) / 30))
+      console.log('GIỜ' + Math.floor(Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24) * 24))
+      console.log('PHÚT' + Math.floor(Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24) * 24 * 60))
+      console.log('GIÂY' + Math.floor(Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24) * 24 * 60 * 60))
       countTime();
 
       function countTime() {
             let today = new Date();
             let sec = today.getSeconds();
-            rootTime.textContent = `${today.getHours()}:${today.getMinutes()}:${(sec > 9) ? sec : "0" + sec}`
+            let minute = today.getMinutes();
+            let hour = today.getHours();
+            rootTime.textContent = `${(hour > 9) ? hour : "0" + hour}:${(minute > 9) ? minute : "0" + minute}:${(sec > 9) ? sec : "0" + sec}`
       }
 
       setInterval(countTime, 1000);
