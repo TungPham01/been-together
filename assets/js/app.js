0ee035b4ce2d1f79f19543f0af8b85b58d789423
Tung Pham <phamduytung11@gmail.com>
2022-09-09 16:49:44 +0700

    improve folder

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,12 +21,6 @@ document.addEventListener('DOMContentLoaded', function () {
             // + hourDate + " Giờ - "
             // + minuteDate + " Phút - "
             // + secondDate + " Giây";
-
-      console.log('NĂM' + Math.floor(Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24) / 365))
-      console.log('THÁNG' + Math.floor(Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24) / 30))
-      console.log('GIỜ' + Math.floor(Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24) * 24))
-      console.log('PHÚT' + Math.floor(Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24) * 24 * 60))
-      console.log('GIÂY' + Math.floor(Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24) * 24 * 60 * 60))
       countTime();
 
       function countTime() {
@@ -39,8 +33,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
       setInterval(countTime, 1000);
       document.querySelector("anni").textContent = `${(yourDate.getDate() > 9) ? yourDate.getDate() : "0" + yourDate.getDate()}-${(yourDate.getMonth() > 8) ? (yourDate.getMonth() + 1) : "0" + (yourDate.getMonth() + 1)}-${yourDate.getFullYear()}`;
-
-      document.querySelector("audio").setAttribute("src", "https://cdn.glitch.me/ccab313a-a6b7-44fd-b482-63acce04fc67/BuocQuaMuaCoDon-Vu-6879419.mp3?v=1641068492024");
 
       document.getElementsByTagName("body")[0].insertAdjacentHTML(
             "beforeend",
